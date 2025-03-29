58c8f3c743cdf20e1433bc3b6165f62f90bfbe3d
Randy <rtang@wyze.com>
2025-03-29 16:24:34 -0700

    change from .gradle to .gradle.kts style

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    `java-gradle-plugin`
+    `maven-publish`
+}
+
+group = "com.sweetraingarden.gplugin"
+val artifactId = "moduledot"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(gradleApi())
+    implementation("org.jgrapht:jgrapht-core:1.5.1")
+    implementation("org.jgrapht:jgrapht-io:1.5.1")
+    
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.1")
+    testImplementation("org.mockito:mockito-core:5.10.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.10.0")
+}
+
+tasks.withType<Copy>().configureEach {
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+gradlePlugin {
+    plugins {
+        create("moduledot") {
+            id = "$group.$artifactId"
+            implementationClass = "com.sweetraingarden.gplugin.moduledot.ModuleDotPlugin"
+            displayName = "Module Dependency Graph Generator"
+            description = "Generates a DOT graph of module dependencies in an Gradle project"
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = group.toString()
+            artifactId = artifactId
+            version = version
+        }
+    }
+    repositories {
+        maven {
+            url = uri("https://github.com/SweetRainGarden/SweetraingardenGradleModuledot.git")
+        }
+    }
+} 
