2898059a3813fc8603c51ad2d908d1ae5bd8f516
Randy <rtang@wyze.com>
2025-03-29 17:10:02 -0700

    Initialize Gradle project and configure repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,10 +14,6 @@ java {
     }
 }
 
-repositories {
-    mavenCentral()
-}
-
 dependencies {
     implementation(gradleApi())
     implementation("org.jgrapht:jgrapht-core:1.5.1")
@@ -57,14 +53,16 @@ gradlePlugin {
 publishing {
     publications {
         create<MavenPublication>("maven") {
-            groupId = group.toString()
-            artifactId = artifactId
-            version = version
+            from(components["java"])
+            groupId = "com.sweetraingarden.gplugin"
+            artifactId = "moduledot"
+            version = "1.0.0"
         }
     }
     repositories {
         maven {
-            url = uri("https://github.com/SweetRainGarden/SweetraingardenGradleModuledot.git")
+            name = "local"
+            url = uri("${buildDir}/repo")
         }
     }
 }
---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+rootProject.name = "SweetraingardenGradleModuledot"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("com.gradle.enterprise") version "3.16.2"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        mavenCentral()
+        google()
+    }
+} 
